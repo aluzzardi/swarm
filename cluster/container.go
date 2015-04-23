19c486dc32bba4f0e7289758b2a2e673cb6fe226
Andrea Luzzardi <aluzzardi@gmail.com>
2015-04-22 17:46:20 -0700

    container: Add helpers to do common operations to containers.
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- cluster/container.go
+++ cluster/container.go
@@ -1,6 +1,8 @@
 package cluster
 
-import "github.com/samalba/dockerclient"
+import (
+	"github.com/samalba/dockerclient"
+)
 
 // Container is exported
 type Container struct {
@@ -8,4 +10,34 @@ type Container struct {
 
 	Info   dockerclient.ContainerInfo
 	Engine *Engine
+}
+
+// Start the container.
+func (c *Container) Start() error {
+	return c.Engine.client.StartContainer(c.Id, nil)
+}
+
+// Stop the container.
+func (c *Container) Stop() error {
+	return c.Engine.client.StopContainer(c.Id, 8)
+}
+
+// Restart the container.
+func (c *Container) Restart(timeout int) error {
+	return c.Engine.client.RestartContainer(c.Id, timeout)
+}
+
+// Kill the container.
+func (c *Container) Kill(signal string) error {
+	return c.Engine.client.KillContainer(c.Id, signal)
+}
+
+// Pause the container.
+func (c *Container) Pause() error {
+	return c.Engine.client.PauseContainer(c.Id)
+}
+
+// Unpause the container.
+func (c *Container) Unpause() error {
+	return c.Engine.client.UnpauseContainer(c.Id)
 }
